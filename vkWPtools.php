3b7a4be746cd510d6802dcba28450820c7e65adf
vicente <desarrollogis@gmail.com>
2013-02-20 00:05:49 -0700

    options page

--- vkWPtools.php
+++ vkWPtools.php
@@ -8,4 +8,19 @@ Author: desarrollogis
 Author URI: http://desarrollogis.dyndns.org/
 License: GPL2
 */
+
+function vkWPtools_options() {
+	if (!current_user_can('manage_options')) {
+		wp_die(__('You do not have sufficient permissions to access this page.'));
+	}
+	echo '<div class="wrap">';
+	echo '<p>Here is where the form would go if I actually had options.</p>';
+	echo '</div>';
+}
+
+function vkWPtools_menu() {
+	add_options_page('vkWPtools Options', 'vkWPtools', 'manage_options', 'vkWPtools', 'vkWPtools_options');
+}
+
+add_action('admin_menu', 'vkWPtools_menu');
 ?>
