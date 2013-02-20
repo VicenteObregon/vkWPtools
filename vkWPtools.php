baa3f61de5598b0d2b86fa7eb8b213e30a4da803
vicente <desarrollogis@gmail.com>
2013-02-19 23:43:25 -0700

    header

---
+++ vkWPtools.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: vkWPtools
+Plugin URI: http://desarrollogis.dyndns.org/
+Description: vkWPtools Plugin.
+Version: 1.0
+Author: desarrollogis
+Author URI: http://desarrollogis.dyndns.org/
+License: GPL2
+*/
+?>
