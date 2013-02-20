22df3c75dc63e3b4dbe58f55ec3578d86bc29f98
vicente <desarrollogis@gmail.com>
2013-02-20 01:10:33 -0700

    gui to backup themes

--- vkWPtools.php
+++ vkWPtools.php
@@ -9,18 +9,28 @@ Author URI: http://desarrollogis.dyndns.org/
 License: GPL2
 */
 
+if (!defined('WP_CONTENT_DIR'))
+	define('WP_CONTENT_DIR', ABSPATH . 'wp-content');
+mkdir(WP_CONTENT_DIR . '/vkWPtools');
+mkdir(WP_CONTENT_DIR . '/vkWPtools/backups');
+mkdir(WP_CONTENT_DIR . '/vkWPtools/backups/themes');
+
 function vkWPtools_options() {
 	if (!current_user_can('manage_options')) {
 		wp_die(__('You do not have sufficient permissions to access this page.'));
 	}
-	echo '<div class="wrap">';
-	echo '<p>Here is where the form would go if I actually had options.</p>';
-	echo '</div>';
+	include_once(dirname(__FILE__) . '/admin/options.php');
 }
 
 function vkWPtools_menu() {
 	add_options_page('vkWPtools Options', 'vkWPtools', 'manage_options', 'vkWPtools', 'vkWPtools_options');
 }
 
+function vkWPtools_backup_theme() {
+	$theme = $_POST['theme'];
+	die($theme);
+}
+
 add_action('admin_menu', 'vkWPtools_menu');
+add_action('wp_ajax_vkWPtools_backup_theme', 'vkWPtools_backup_theme');
 ?>
