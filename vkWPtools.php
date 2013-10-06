d01d8e3eb7cac5d0651db3a54f37c741af546643
Vicente Obregon <desarrollogis@gmail.com>
2013-10-05 18:23:54 -0700

    backwards

--- vkWPtools.php
+++ vkWPtools.php
@@ -9,21 +9,27 @@ Author URI: http://desarrollogis.dyndns.org/
 License: GPL2
 */
 
-if (!defined('vkWPtools_DIR'))
+if (!defined('vkWPtools_DIR')) {
 	define('vkWPtools_DIR', ABSPATH . 'wp-content/plugins/vkWPtools/');
-if (!defined('vkWPtools_URL'))
+}
+if (!defined('vkWPtools_URL')) {
 	define('vkWPtools_URL', content_url() . '/plugins/vkWPtools/');
+}
 $directories = array();
 $directories[] = vkWPtools_DIR . 'backups';
 $directories[] = vkWPtools_DIR . 'backups/themes';
 foreach ($directories as $directory) {
-	if (!is_dir($directory))
+	if (!is_dir($directory)) {
 		mkdir($directory);
+	}
+}
+
+function vkWPtools_admin_enqueue_scripts() {
+	wp_enqueue_script('vkWPtools', vkWPtools_URL . 'vkWPtools.js', array('jquery'));
 }
 
 function vkWPtools_options() {
 	current_user_can('manage_options') or wp_die(__('Insufficient privileges.'));
-	wp_enqueue_script('vkWPtools', vkWPtools_URL . 'vkWPtools.js', array('jquery'));
 	include_once(vkWPtools_DIR . 'vkWPtools_options.php');
 }
 
@@ -37,7 +43,7 @@ function vkWPtools_clean_backups() {
 	chdir($directory);
 	$command = "rm -f *.zip";
 	exec($command);
-	wp_die('Ok');
+	die('Ok');
 }
 
 function vkWPtools_backup_theme() {
@@ -49,7 +55,7 @@ function vkWPtools_backup_theme() {
 	$command = "zip -r \"{$filename}\" {$theme} -x {$theme}/.git\\* {$theme}\\*.sh";
 	exec($command);
 	$url = vkWPtools_URL . "backups/themes/{$theme}.zip";
-	wp_die($url);
+	die($url);
 }
 
 function vkWPtools_backup_pages_name() {
@@ -86,6 +92,7 @@ function vkWPtools_delete_key() {
 	wp_die('Ok');
 }
 
+add_action('admin_enqueue_scripts', 'vkWPtools_admin_enqueue_scripts');
 add_action('admin_menu', 'vkWPtools_menu');
 add_action('wp_ajax_vkWPtools_backup_theme', 'vkWPtools_backup_theme');
 add_action('wp_ajax_vkWPtools_clean_backups', 'vkWPtools_clean_backups');
--- vkWPtools_options.php
+++ vkWPtools_options.php
@@ -6,43 +6,60 @@ foreach ($posts as $post) {
 		$keys[$value] = 0;
 	}
 }
+$themes = array();
+if (function_exists('wp_get_themes')) {
+	foreach (wp_get_themes(array('errors' => null, 'allowed' => null)) as $theme) {
+		$themes[] = array($theme->Template, $theme->Name);
+	}
+}
+else {
+	$dir = ABSPATH . 'wp-content/themes';
+	$handle = opendir($dir);
+	while ($name = readdir($handle)) {
+		if (($name[0] == '.') || !is_dir("$dir/$name")) {
+			continue;
+		}
+		$themes[] = array($name, $name);
+	}
+	closedir($handle);
+}
 ?>
 <div class="wrap">
-	<div id="icon-options-general" class="icon32">
-		<br />
-	</div>
-	<h2>vkWPtools</h2>
-	<table class="form-table">
-	<tr>
-		<th><label for="theme_to_backup">Backup theme:</label></th>
-		<td>
-			<select id="theme_to_backup">
-				<?php foreach (wp_get_themes(array('errors' => null, 'allowed' => null)) as $theme): ?>
-				<option value="<?php echo $theme->Template; ?>"><?php echo $theme->Name; ?></option>
-				<?php endforeach; ?>
-			</select>
-			<button id="backup_theme" class="button">Backup</button>
-			<button id="clean_backups" class="button">Clean backups</button>
-		</td>
-	</tr>
-	<tr>
-		<th><label>Backup pages name:</label></th>
-		<td>
-			<button id="backup_pages_name" class="button">Backup</button>
-			<button id="restore_pages_name" class="button">Restore</button>
-		</td>
-	</tr>
-	<tr>
-		<th><label for="key_to_delete">Delete key:</label></th>
-		<td>
-			<select id="key_to_delete">
-				<option value="">--Select a key--</option>
-				<?php foreach ($keys as $key => $value): ?>
-				<option value="<?php echo $key; ?>"><?php echo $key; ?></option>
-				<?php endforeach; ?>
-			</select>
-			<button id="delete_key" class="button">Delete</button>
-		</td>
-	</tr>
-	</table>
+<div id="icon-options-general" class="icon32">
+<br />
+</div>
+<h2>vkWPtools</h2>
+<table class="form-table">
+<tr>
+<th><label for="theme_to_backup">Backup theme:</label></th>
+<td>
+<select id="theme_to_backup">
+<?php foreach ($themes as $theme): ?>
+<option value="<?php echo $theme[0]; ?>"><?php echo $theme[1]; ?></option>
+<?php endforeach; ?>
+</select>
+<button id="backup_theme" class="button">Backup</button>
+<button id="clean_backups" class="button">Clean backups</button>
+</td>
+</tr>
+<tr>
+<th><label>Backup pages name:</label></th>
+<td>
+<button id="backup_pages_name" class="button">Backup</button>
+<button id="restore_pages_name" class="button">Restore</button>
+</td>
+</tr>
+<tr>
+<th><label for="key_to_delete">Delete key:</label></th>
+<td>
+<select id="key_to_delete">
+<option value="">--Select a key--</option>
+<?php foreach ($keys as $key => $value): ?>
+<option value="<?php echo $key; ?>"><?php echo $key; ?></option>
+<?php endforeach; ?>
+</select>
+<button id="delete_key" class="button">Delete</button>
+</td>
+</tr>
+</table>
 </div>
